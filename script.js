caffb4fb74ea33db72ec3ff7e22bc03038ddb00c
Matthew Macomber <4676454+MatthewMacomber@users.noreply.github.com>
2020-11-10 11:55:44 -0800

    worked on grammar and portfolio descriptions

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ let projects = [
   {
     name: 'GOAT',
     picture: 'images/goat-site.png',
-    description: 'A gamified goal oriented task management tool.',
-    skills: 'Javascript, Design, Node.js, Express, React, PostgreSQL',
+    description: 'A gamified goal-oriented task management tool. Created to help people keep track of and complete tasks in their day to day life.',
+    skills: 'JavaScript, Design, Node.js, Express, React, PostgreSQL',
     githubLink: 'https://github.com/MatthewMacomber/goat-client',
     liveLink: 'https://goat-client.vercel.app/',
   },
   {
     name: 'The Codes',
     picture: 'images/thecodes-site.png',
-    description: 'A website for sharing and solving text based codes and ciphers.',
-    skills: 'Javascript, Node.js, text-to-image, Express, React, Design, SQL',
+    description: 'A website for sharing and solving text-based codes and ciphers. Developed for everyone who enjoys creating and solving codes.',
+    skills: 'JavaScript, Node.js, text-to-image, Express, React, Design, SQL',
     githubLink: 'https://github.com/MatthewMacomber/TheCodes-client',
     liveLink: 'https://thecodes-client.vercel.app/',
   },
@@ -20,7 +20,7 @@ let projects = [
     name: 'Cyberdeck',
     picture: 'images/cyberdeckPlaceholder.png',
     description: 'This project is all about building a complex upgradable \'cyberdeck\' in the styles given by the book Necromancer by William Gibson.',
-    skills: '3D Modeling, Electronics Design and Assembly, Assembly, Lua and Python Programming',
+    skills: '3D Modeling, Electronics Design and Assembly, Assembly, Lua, and Python Programming',
     githubLink: 'http://github.com/MatthewMacomber/CyberDeck',
     liveLink: 'http://github.com/MatthewMacomber/CyberDeck',
   },
@@ -28,7 +28,7 @@ let projects = [
 
 // Inserts each project object from the projects array into the proper part of the website as a unordered list.
 for (let i = 0; i < projects.length; i++) {
-  document.getElementById('projects').innerHTML += '<li><h3>' + projects[i].name + '</h3><br><button onclick="showHide(' + i + ')"><img src="' + projects[i].picture + '"></button>' + '<div id="' + i + '" style="display: block"><p>' + projects[i].description + '</p><p id="built">Built with: ' + projects[i].skills + '<p>Github: <a href="' + projects[i].githubLink + '">' + projects[i].githubLink + '</a></p><p>Live/Demo: <a href="' + projects[i].liveLink + '">' + projects[i].liveLink + '</a></p></div>' + '</li>';
+  document.getElementById('projects').innerHTML += '<li><h3>' + projects[i].name + '</h3><br><button onclick="showHide(' + i + ')"><img src="' + projects[i].picture + '"></button>' + '<div id="' + i + '" style="display: block"><p>' + projects[i].description + '</p><p id="built">Built with: ' + projects[i].skills + '<p>GitHub: <a href="' + projects[i].githubLink + '">' + projects[i].githubLink + '</a></p><p>Live/Demo: <a href="' + projects[i].liveLink + '">' + projects[i].liveLink + '</a></p></div>' + '</li>';
 }
 
 // eslint-disable-next-line no-unused-vars
